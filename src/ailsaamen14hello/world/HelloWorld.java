205f54155f4ac64948f561214941b33670c1dd5a
ailsa.mensinger <ailsa.mensinger@CLC-LAB209-03.TCSD.tooelesd.org>
2018-09-04 13:24:13 -0600

    first commit

---
+++ src/ailsaamen14hello/world/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * hello world program print hello world in console
+ */
+package ailsaamen14hello.world;
+
+/**
+ * @author ailsa.mensinger
+ *9/18
+ *hello world
+ */
+public class HelloWorld {
+
+	
+	public HelloWorld() {
+		
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+	}
+
+}
